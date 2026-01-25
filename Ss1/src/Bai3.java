250a4066d4de7182db36acf167b8fb6aa2c89762
Tô Anh Tuấn <toanhtuan.18dz@gmail.com>
2026-01-25 18:10:08 +0700

    ss1

--- Ss1/src/Bai3.java
+++ Ss1/src/Bai3.java
@@ -14,14 +14,5 @@ public class Bai3 {
         System.out.println("Trước khi hoán đổi: Book 1 = " + book1 + ", Book 2 = " + book2);
         System.out.println("Sau khi hoán đổi: Book 1 = " + book2 + ", Book 2 = " + book1);
 
-        // xuất dữ liệu
-        System.out.print("Hello"); // ko xuống dòng
-        System.out.print("Hello\n"); // xuống dòng thủ công
-        System.out.println("World"); // tự động xuống dòng
-        // in theo định dạng
-        // kí tự đại diện:  %s(string), %c(char), %d(int, byte, short, long), %f(float, double)
-        // kí tự điều khiển: \n, %n (xuống dòng), \t (tab)
-        System.out.printf("In định dạng: %nTen tôi là %s, năm nay tôi %d tuổi", "Tuan",18);
-
     }
 }
